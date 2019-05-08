3590ebe3ca2158d49ffd90c3ea221a42be72c441
Nabila <nabilatajrin@gmail.com>
2019-05-08 10:32:04 +0600

    10:31AM 08MAY2019 update

--- java-programs/src/variables/BigDecimal.java
+++ java-programs/src/variables/BigDecimal.java
@@ -1,5 +1,8 @@
 package variables;
 
 public class BigDecimal {
-    
+    public static void main(String[] args){
+        double value = .012;
+        System.out.println("double value = " + value);
+    }
 }
