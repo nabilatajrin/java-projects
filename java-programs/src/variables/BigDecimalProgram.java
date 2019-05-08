3354c0ae4f4ef4ed48b8ee1fbd9df991dbddaf20
Nabila <nabilatajrin@gmail.com>
2019-05-08 13:21:53 +0600

    1:21PM 08MAY2019 update

--- java-programs/src/variables/BigDecimalProgram.java
+++ java-programs/src/variables/BigDecimalProgram.java
@@ -4,23 +4,23 @@ import java.math.BigDecimal;
 
 public class BigDecimalProgram {
 
-    public BigDecimalProgram(int par) {
-    }
     public static void main(String[] args){
         double value = .012;
         System.out.println("value = " + value);
         
         double pSum = value + value + value;
-        System.out.println("\npSum = " + pSum);
+        System.out.println("pSum = " + pSum);
         
         String strValue = Double.toString(value);
         strValue = "0.012";
         
         BigDecimal bigValue = new BigDecimal(strValue);
-        System.out.println("bigValue = " + bigValue);
+        System.out.println("\nbigValue = " + bigValue);
         
-       
+        BigDecimal bSum = bigValue.add(bigValue).add(bigValue);
+        System.out.println("bSum = " + bSum);
+        
+        Double sum = bSum.doubleValue();
+        System.out.println("sum = " + sum);  
     }
-
-  
 }
