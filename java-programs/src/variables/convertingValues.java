b4c1141bad309ee575f338b37c085768f7513355
Nabila <nabilatajrin@gmail.com>
2019-05-09 11:49:34 +0600

    11:49AM 09MAY2019 update

--- java-programs/src/variables/convertingValues.java
+++ java-programs/src/variables/convertingValues.java
@@ -11,5 +11,14 @@ public class convertingValues {
        short sh2 = (short)i;
        System.out.println("sh2 = " + sh2);
        
+       short sh3 = 3000;
+       byte b1 = (byte)sh3;
+       System.out.println("b1 = " + b1);
+       
+       double doubleValue = 3.99999d; //d is a bit of literal syntax saying hey compiler it must be a double value
+       System.out.println("doubleValue = " + doubleValue);
+       
+       int intValue = (int)doubleValue; //double value truncated to int
+       System.out.println("intValue = " + intValue);
     }
 }
