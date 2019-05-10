af6fc1a2fab997557618e4264624d5b9c6bce773
Nabila <nabilatajrin@gmail.com>
2019-05-10 18:26:44 +0600

    6:26PM 10MAY2019 update

---
+++ java-programs/src/variables/CharacterValues.java
@@ -0,0 +1,13 @@
+package variables;
+
+public class CharacterValues {
+    public static void main(String[] args){
+        char c1 = '1';
+        char c2 = '2';
+        char c3 = '3';
+        char nine = '\u0039'; //unicode
+        System.out.println("nine = " + nine);
+        char dollar = '\u0024'; //unicode
+        System.out.println("dollar = " + dollar);
+    }
+}
