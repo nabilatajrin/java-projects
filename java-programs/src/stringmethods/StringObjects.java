93787b167b490f4a1acaa40f69ad25947c9d0115
Nabila <nabilatajrin@gmail.com>
2019-05-12 14:44:52 +0600

    02:44PM 12MAY2019 update

--- java-programs/src/stringmethods/StringObjects.java
+++ java-programs/src/stringmethods/StringObjects.java
@@ -9,6 +9,19 @@ public class StringObjects {
         StringObjects item2 = new StringObjects();
         item2.type = "shoe";
         item2.displayItem();
+        
+        //string is an object example
+        String string1 = new String("Hello!");
+        String string2 = "Hello2!";
+        System.out.println(string1);
+        System.out.println(string2);//same result
+        string1 = "hey";
+        System.out.println(string1);
+        
+        //another example
+        char[] chars = {'J','e','l','l','o','!'};
+        String s = new String(chars);
+        System.out.println(s);             
     }
     
     private void displayItem(){
