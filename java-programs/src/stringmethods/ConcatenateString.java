3ccf721b092a63008efe92e7681a01c00b62664f
Nabila <nabilatajrin@gmail.com>
2019-05-12 16:45:19 +0600

    04:44PM 12MAY2019 update

---
+++ java-programs/src/stringmethods/ConcatenateString.java
@@ -0,0 +1,26 @@
+package stringmethods;
+
+public class ConcatenateString {
+    public static void main(String[] args){
+        String s1 = "This is a string";
+        String s2 = new String("This is also a string");
+        
+        char[] chars = {'h','e','l','l','o'};
+        
+        String s3 = new String(chars);
+        
+        System.out.println(s3);
+        
+        String s = "shirt size: ";
+        String size = "M";
+        String description = s + size;
+        System.out.println(description);
+        
+        description += ", very elegant";
+        System.out.println(description);
+        System.out.println(description.toUpperCase());
+        
+        System.out.println(s.charAt(4));
+        System.out.println(s.getBytes());
+    }
+}
