610dc3f3fa2b08ddb04b74760d115b9b6a025a14
Nabila <nabilatajrin@gmail.com>
2019-05-13 11:33:53 +0600

    11:33AM 13MAY2019 update

--- java-programs/src/stringmethods/ConvertPrimitiveValueToString.java
+++ java-programs/src/stringmethods/ConvertPrimitiveValueToString.java
@@ -10,12 +10,35 @@ public class ConvertPrimitiveValueToString {
         String howMany = 20 + " things";
         System.out.println(howMany);
         
+        //int to string
         int intValue = 42;
         String fromInt = Integer.toString(intValue);
         System.out.println(intValue);
         
+        //boolean to string
         Boolean boolValue = true;
         String fromBool = Boolean.toString(boolValue);
         System.out.println(fromBool);
+        
+        //long to string
+        long longValue = 10_000_000;//these underscores are for code readability
+        System.out.println(longValue);
+        
+        String fromLong = Long.toString(longValue);
+        System.out.println(fromLong);
+        
+        //string Interpolation is a programming technique that is available 
+        //in a quite few languages. Ex- Kotlin does this automatically.
+        //This is string interpolation, it's a format method
+        String stringInterpolation = 
+                String.format("Shirt size: %s, Qty: %d", "M", 4);
+        System.out.println(stringInterpolation);
+        
+        //or
+        
+        int qty = 5;
+        String size = "L";
+        String result2 = String.format("Shirt size: %s, Qty: %d", size, qty);
+        System.out.println(result2);
     }
 }
