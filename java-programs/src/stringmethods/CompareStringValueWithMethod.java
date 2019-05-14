2db28d66cfc9e43f0cd9431ae1b5b99f0ad42abb
Nabila <nabilatajrin@gmail.com>
2019-05-14 15:30:04 +0600

    3:29PM 14MAY2019 update

---
+++ java-programs/src/stringmethods/CompareStringValueWithMethod.java
@@ -0,0 +1,45 @@
+package stringmethods;
+
+public class CompareStringValueWithMethod {
+    public static void main(String[] args){
+        
+        //literal value. Strings are object
+        //process: interning
+        String s1 = "Hello";
+        String s2 = "Hello";
+        
+        if(s1 == s2){
+            System.out.println("They match!");
+        }else{
+            System.out.println("They don't match..");
+        }
+        
+        String s3 = new String("Hello!");
+        String s4 = new String("Hello!");
+        
+        //this doesn't work in this case
+        if(s3 == s4){
+            System.out.println("They match!");
+        }else{
+            System.out.println("They don't match..");
+        }
+        
+        //this works
+        if(s3.equals(s4)){
+            System.out.println("They match!");
+        }else{
+            System.out.println("They don't match..");
+        }
+        
+        
+        String s5 = new String("HeLLo!");
+        String s6 = new String("Hello!");
+        if(s3.equalsIgnoreCase(s4)){
+            System.out.println("They match!");
+        }else{
+            System.out.println("They don't match..");
+        }
+        
+        
+    }
+}
