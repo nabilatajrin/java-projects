20e37602b30914c9fec97fb8b6aa8b32e8730418
Nabila <nabilatajrin@gmail.com>
2019-05-14 12:07:20 +0600

    12:07PM 14MAY2019 update

---
+++ java-programs/src/stringmethods/StringBuilderTest.java
@@ -0,0 +1,8 @@
+package stringmethods;
+
+public class StringBuilderTest {
+    public static void main(String[] args){
+        StringBuilder sb = new StringBuilder("Welcome");
+        System.out.println(sb);
+    }
+}
