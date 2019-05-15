65fdaf80014eaf59a2809054da5513ed4a813632
Nabila <nabilatajrin@gmail.com>
2019-05-15 15:03:13 +0600

    3:03PM 15MAY2019 update

--- java-programs/src/otherjavaprograms/ScannerTest.java
+++ java-programs/src/otherjavaprograms/ScannerTest.java
@@ -1,14 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package otherjavaprograms;
 
-/**
- *
- * @author iit
- */
+import java.awt.*;
+import java.util.Scanner;
+
 public class ScannerTest {
     
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("write something");
+        String userInput = sc.next();
+        
+        String uppercased = userInput.toUpperCase();
+        System.out.println(uppercased);
+        
+        char firstCharacter = userInput.charAt(0);
+        System.out.println(firstCharacter);
+        
+        System.out.println("Contains: " + userInput.contains("Enter"));
+        System.out.println("Contains: " + userInput.contains("nter"));
+        
+        System.out.println("Type a number: ");
+        int userNumber = sc.nextInt();
+        System.out.println(userNumber);
+        double userNumber2 = sc.nextDouble();
+        System.out.println(userNumber2);
+    }
 }
