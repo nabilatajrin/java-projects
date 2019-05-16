4226a735718d8d6f689ca0bd8fc3a8a7bb614999
Nabila <nabilatajrin@gmail.com>
2019-05-16 10:59:18 +0600

    10:59AM 16MAY2019 update

--- java-programs/src/arraymethods/arrayBasic.java
+++ java-programs/src/arraymethods/arrayBasic.java
@@ -1,14 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package arraymethods;
 
-/**
- *
- * @author iit
- */
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class arrayBasic {
-    
+    public static void main(String[] args){
+        //31,45,22,98,10
+        
+        //allocate seperately
+        int[] numbers = new int[5];
+        
+        numbers[0] = 31;
+        numbers[1] = 45;
+        numbers[2] = 22;
+        numbers[3] = 98;
+        numbers[4] = 10;
+        
+        //sort numbers, this method is mutating the numbers of the array
+        Arrays.sort(numbers);
+        System.out.println("sorted numbers: " + numbers); //[I@15db9742 is memory address
+        System.out.println("sorted numbers: " + Arrays.toString(numbers));
+        
+        //declaring, initializing, allocating in one line of code
+        int[] numbers2 = {31, 45, 22, 98, 10};
+        
+        String[] myFavoriteCandyBars = {"twix", "Hersey's", "Crunch"};
+        System.out.println("Index 1: " + myFavoriteCandyBars[1]);
+        
+        //replace
+        myFavoriteCandyBars[2] = "Butterfingers";
+        System.out.println("Index 2: " + myFavoriteCandyBars[2]);
+        
+        //get the length of the array, most used
+        System.out.println("Length: " + myFavoriteCandyBars.length);
+
+        //array class
+        System.out.println("Print: " + Array.get(myFavoriteCandyBars, 2));
+        
+    }
 }
