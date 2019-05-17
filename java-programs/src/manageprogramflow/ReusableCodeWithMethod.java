2b95e5018c4ea55e6c82d510d119d30beb36efa3
Nabila <nabilatajrin@gmail.com>
2019-05-17 07:57:19 +0600

    07:55AM 17MAY2019 update

--- java-programs/src/manageprogramflow/ReusableCodeWithMethod.java
+++ java-programs/src/manageprogramflow/ReusableCodeWithMethod.java
@@ -5,10 +5,51 @@
  */
 package manageprogramflow;
 
-/**
- *
- * @author iit
- */
+import java.util.Scanner;
+
 public class ReusableCodeWithMethod {
     
+    //array of Strings
+    private static String[] months = 
+    {"January", "February", "March",
+        "April", "May", "June",
+        "July", "August", "September",
+        "October", "November", "December"};
+    
+    public static void main(String[] args){ 
+        loopMonths();
+        loopMonths();
+        
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            System.out.println("\nEnter a month: ");
+            String monthName = sc.nextLine();
+            if(monthName.length() == 0){
+                break;
+            }
+            int monthNumber = getMonthNumber(monthName);
+            if(monthNumber == -1){
+                System.out.println("month not found!");
+            }else{
+                System.out.println(String.format(
+                        "%s is month number %d", monthName, monthNumber + 1));
+            }
+        }
+    }
+    
+    private static void loopMonths(){
+        System.out.println("\nFor loop:");
+        for (int i = 0; i < months.length; i++){
+            System.out.println(months[i]);
+        }
+    }
+    
+    private static int getMonthNumber(String monthName){
+        //System.out.println("\nFor loop:");
+        for (int i = 0; i < months.length; i++){
+            if(monthName.equalsIgnoreCase(months[i]))
+                return i;
+        }
+        return -1;
+    }
 }
--- java-programs/src/manageprogramflow/loopingCodeBlocks.java
+++ java-programs/src/manageprogramflow/loopingCodeBlocks.java
@@ -10,25 +10,37 @@ public class loopingCodeBlocks {
         "October", "November", "December"};
     
     public static void main(String[] args){
-        //for loop
+        //1- for loop/ iteration loop
         System.out.println("For loop:");
         for (int i = 0; i < months.length; i++){
             System.out.println(months[i]);
         }
         
-        //for each loop
+        //2- for each loop
         System.out.println("\nFor each loop:");
         for (String month:
             months){
             System.out.println(month);
         }
         
-        //while loop
+        //3- while loop
         System.out.println("\nWhile loop:");
         int counter = 0;//need to declare before
         while(counter < months.length){
             System.out.println(months[counter]);
             counter++;
         }
+        
+        //4- do while loop
+        System.out.println("\nDo while loop:");
+        int counter2 = 0;
+        do{
+            System.out.println(months[counter]);
+            counter++;
+            
+            if(counter == 5){
+                break;
+            }
+        }while(counter < months.length);
     } 
 }
