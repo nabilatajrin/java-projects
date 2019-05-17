7ea82944571494653db13b224b8585a40563cb24
Nabila <nabilatajrin@gmail.com>
2019-05-17 14:26:46 +0600

    02:26PM 17MAY2019 update

---
+++ java-programs/src/stringmethods/ReplaceString.java
@@ -0,0 +1,9 @@
+package stringmethods;
+
+public class ReplaceString {
+    public static void main(String[] args){
+        String s = "dog";
+        String replacedF = s.replace('d', 'f');
+        System.out.println(replacedF);
+    }
+}
