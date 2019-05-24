917de570073361fdec9ad8ff8c95fddaaf4481bf
Nabila <nabilatajrin@gmail.com>
2019-05-24 14:10:20 +0600

    02:10PM 24MAY2019 update

--- java-programs/src/manageprogramflow/AcomplexCalculator.java
+++ java-programs/src/manageprogramflow/AcomplexCalculator.java
@@ -1,14 +1,62 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package manageprogramflow;
 
-/**
- *
- * @author iit
- */
+import java.util.Scanner;
+
 public class AcomplexCalculator {
+    public static void main(String[] args){
+        String s1 = getInput("Enter a numeric value: ");
+        String s2 = getInput("Enter a numeric value: ");
+        String op = getInput("Choose an operation(+ - * %): ");
+        
+        double result = 0;
+        
+        switch (op){
+            case "+":
+                result = addValues(s1, s2);
+                break;
+            case "-":
+                result = subtractValues(s1, s2);
+                break;
+            case "*":
+                result = multiplyValues(s1, s2);
+                break;
+            case "/":
+                result = divideValues(s1, s2);
+                break;
+            default:
+                System.out.println("Unrecognized operation!");
+                return;
+        }
+        System.out.println("The answer is: " + result);
+    }
+
+    private static String getInput(String prompt) {
+        System.out.println(prompt);
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
+    }
+
+    private static double addValues(String s1, String s2) {
+        double d1 = Double.parseDouble(s1);
+        double d2 = Double.parseDouble(s2);
+        return d1 + d2;
+    }
     
+    private static double subtractValues(String s1, String s2) {
+        double d1 = Double.parseDouble(s1);
+        double d2 = Double.parseDouble(s2);
+        return d1 - d2;
+    }
+    
+    private static double multiplyValues(String s1, String s2) {
+        double d1 = Double.parseDouble(s1);
+        double d2 = Double.parseDouble(s2);
+        return d1 * d2;
+    }
+    
+    private static double divideValues(String s1, String s2) {
+        double d1 = Double.parseDouble(s1);
+        double d2 = Double.parseDouble(s2);
+        return d1 / d2;
+    }
 }
