d4223d67a7cd33c129daefe81d6c61b0d2dea299
Nabila <nabilatajrin@gmail.com>
2019-05-27 13:01:27 +0600

    1:01PM 27MAY2019 update

--- java-programs/src/ExceptionHandlingAndDebugging/SyntaxErrorVsExceptions.java
+++ java-programs/src/ExceptionHandlingAndDebugging/SyntaxErrorVsExceptions.java
@@ -1,14 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ExceptionHandlingAndDebugging;
 
-/**
- *
- * @author iit
- */
 public class SyntaxErrorVsExceptions {
-    
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+        int x = 1;
+        //String x = "Hello"; //this causes syntax err. to solve this do the following:
+        String y = "Hello";
+        String welcome = "Welcome!";
+        char[] chars = welcome.toCharArray();
+        char lastChar = chars[chars.length - 1]; //bcz index starts from 0
+        System.out.println("last char: " + lastChar);
+        
+        //exceptions test
+        String welcome2 = "Welcome!";
+        char[] chars2 = welcome2.toCharArray();
+        char lastChar2 = chars[chars2.length];
+        System.out.println("last char: " + lastChar2);
+        
+        //test
+        String nothing = null;
+        System.out.println(nothing); //okay
+        System.out.println("String length: " + nothing.length());
+    }
 }
